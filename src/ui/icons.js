1450014f3726dc955f3172150703809bca108822
4teen Genesis <info14fourteen@gmail.com>
2026-03-17 16:25:57 +0500

    Create icons.js

---
+++ src/ui/icons.js
@@ -0,0 +1,5 @@
+import trxIcon from '../assets/trx.svg';
+import fourteenIcon from '../assets/4teen.svg';
+import walletIcon from '../assets/wallet.svg';
+
+export { trxIcon, fourteenIcon, walletIcon };
