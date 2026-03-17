de3dbd1c863c563d50a419f88dd8d0875216b7ad
4teen Genesis <info14fourteen@gmail.com>
2026-03-17 16:15:40 +0500

    Create constants.js

---
+++ src/core/config/constants.js
@@ -0,0 +1,10 @@
+export const TRON_MAINNET_CHAIN_ID = 'tron:0x2b6653dc';
+
+export const APP_METADATA = {
+  name: '4TEEN Connect Kit',
+  description: 'Wallet connectivity layer for 4TEEN on TRON',
+  url: 'https://4teen.me',
+  icons: ['https://4teen.me/logo.png']
+};
+
+export const DEFAULT_BALANCE_DECIMALS = 2;
