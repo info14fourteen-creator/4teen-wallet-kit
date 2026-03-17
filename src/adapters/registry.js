76f2786df0d1d56063124e67f444c07a8ca1856a
4teen Genesis <info14fourteen@gmail.com>
2026-03-17 16:22:22 +0500

    Create registry.js

---
+++ src/adapters/registry.js
@@ -0,0 +1,9 @@
+export const WALLET_REGISTRY = {
+  tronlink: { id: 'tronlink', name: 'TronLink' },
+  okx: { id: 'okx', name: 'OKX Wallet' },
+  binance: { id: 'binance', name: 'Binance Wallet' },
+  trust: { id: 'trust', name: 'Trust Wallet' },
+  bitget: { id: 'bitget', name: 'Bitget Wallet' },
+  tokenpocket: { id: 'tokenpocket', name: 'TokenPocket' },
+  metamask: { id: 'metamask', name: 'MetaMask' }
+};
