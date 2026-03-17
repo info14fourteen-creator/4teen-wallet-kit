c548697da7a703ed0f79a4c2d762ae81bb38aee0
4teen Genesis <info14fourteen@gmail.com>
2026-03-17 17:41:05 +0500

    Update createAdapters.js

--- src/adapters/createAdapters.js
+++ src/adapters/createAdapters.js
@@ -1,19 +1,23 @@
 import { TronLinkAdapter } from '@tronweb3/tronwallet-adapter-tronlink';
 import { OkxWalletAdapter } from '@tronweb3/tronwallet-adapter-okxwallet';
-import { BinanceAdapter } from '@tronweb3/tronwallet-adapter-binance';
+import { BinanceWalletAdapter } from '@tronweb3/tronwallet-adapter-binance';
 import { TrustAdapter } from '@tronweb3/tronwallet-adapter-trust';
 import { BitKeepAdapter } from '@tronweb3/tronwallet-adapter-bitkeep';
 import { TokenPocketAdapter } from '@tronweb3/tronwallet-adapter-tokenpocket';
+import { MetaMaskAdapter } from '@tronweb3/tronwallet-adapter-metamask';
 import { WalletConnectAdapter } from '@tronweb3/tronwallet-adapter-walletconnect';
 
 export function createWalletAdapters() {
   return [
     new TronLinkAdapter(),
     new OkxWalletAdapter(),
-    new BinanceAdapter(),
+    new BinanceWalletAdapter(),
     new TrustAdapter(),
     new BitKeepAdapter(),
     new TokenPocketAdapter(),
+    new MetaMaskAdapter({
+      useDeeplink: true
+    }),
     new WalletConnectAdapter({
       network: 'Mainnet',
       options: {
