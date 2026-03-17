00db65c61c60907c8a4877b426105c956f086226
4teen Genesis <info14fourteen@gmail.com>
2026-03-17 17:00:40 +0500

    Update token.js

--- src/core/config/token.js
+++ src/core/config/token.js
@@ -2,5 +2,6 @@ export const FOURTEEN_TOKEN = {
   name: '4TEEN',
   symbol: '4TEEN',
   address: 'TMLXiCW2ZAkvjmn79ZXa4vdHX5BE3n9x4A',
-  decimals: 6
+  decimals: 6,
+  icon: '/src/assets/4teen.svg'
 };
