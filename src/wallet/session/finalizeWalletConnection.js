acddaac8b927de513a43e863df3d1de926411bf4
4teen Genesis <info14fourteen@gmail.com>
2026-03-18 07:01:37 +0500

    Update finalizeWalletConnection.js

--- src/wallet/session/finalizeWalletConnection.js
+++ src/wallet/session/finalizeWalletConnection.js
@@ -1,34 +1,7 @@
 import { getWalletState, setWalletState } from '../../core/store/walletStore.js';
 import { shortenAddress } from '../../core/utils/address.js';
+import { forceBindTronWeb } from '../../adapters/shared/accountRequests.js';
 import { refreshAllBalances } from '../../services/balances/refreshAllBalances.js';
-
-async function forceBindTronWeb(provider, address) {
-  if (!provider || !address) return;
-
-  try {
-    if (typeof provider.setAddress === 'function') {
-      provider.setAddress(address);
-    }
-  } catch (_) {}
-
-  try {
-    if (provider?.tronWeb && typeof provider.tronWeb.setAddress === 'function') {
-      provider.tronWeb.setAddress(address);
-    }
-  } catch (_) {}
-
-  try {
-    if (provider?.defaultAddress && typeof provider.defaultAddress === 'object') {
-      provider.defaultAddress.base58 = address;
-    }
-  } catch (_) {}
-
-  try {
-    if (provider?.tronWeb?.defaultAddress && typeof provider.tronWeb.defaultAddress === 'object') {
-      provider.tronWeb.defaultAddress.base58 = address;
-    }
-  } catch (_) {}
-}
 
 function buildConnectedPatch({ walletId, walletName, address, provider }) {
   return {
