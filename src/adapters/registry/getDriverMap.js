21a9271207746e964442f52bc14624313315d1da
4teen Genesis <info14fourteen@gmail.com>
2026-03-18 08:06:54 +0500

    Update getDriverMap.js

--- src/adapters/registry/getDriverMap.js
+++ src/adapters/registry/getDriverMap.js
@@ -1,14 +1,24 @@
 import { createTronLinkDriver } from '../drivers/tronlink/index.js';
+import { createOkxDriver } from '../drivers/okx/index.js';
+import { createBinanceDriver } from '../drivers/binance/index.js';
+import { createTokenPocketDriver } from '../drivers/tokenpocket/index.js';
+import { createBitgetDriver } from '../drivers/bitget/index.js';
+import { createMetaMaskDriver } from '../drivers/metamask/index.js';
 
 let driverMap = null;
 
 export function getDriverMap() {
-  if (driverMap) return driverMap;
-
-  const tronlink = createTronLinkDriver();
+  if (driverMap) {
+    return driverMap;
+  }
 
   driverMap = {
-    tronlink
+    tronlink: createTronLinkDriver(),
+    okx: createOkxDriver(),
+    binance: createBinanceDriver(),
+    tokenpocket: createTokenPocketDriver(),
+    bitget: createBitgetDriver(),
+    metamask: createMetaMaskDriver()
   };
 
   return driverMap;
