19407cc08e202d8bcdc4763476975afeeaf529df
4teen Genesis <info14fourteen@gmail.com>
2026-03-22 21:01:18 +0500

    Update mobileShell.config.js

--- src/ui/mobileShell.config.js
+++ src/ui/mobileShell.config.js
@@ -1,5 +1,21 @@
 // src/ui/mobileShell.config.js
 
+import facebookSocial from '../assets/socials/facebook_social.svg';
+import xSocial from '../assets/socials/x_social.svg';
+import instagramSocial from '../assets/socials/instagram_social.svg';
+import youtubeSocial from '../assets/socials/youtube_social.svg';
+import whatsappSocial from '../assets/socials/whatsapp_social.svg';
+import telegramSocial from '../assets/socials/telegram_social.svg';
+import threadsSocial from '../assets/socials/threads_social.svg';
+import tiktokSocial from '../assets/socials/tiktok_social.svg';
+import discordSocial from '../assets/socials/discord_social.svg';
+import githubSocial from '../assets/socials/github_social.svg';
+
+import buyMenu from '../assets/menu/buy_menu.svg';
+import swapMenu from '../assets/menu/swap_menu.svg';
+import unlockMenu from '../assets/menu/unlock_menu.svg';
+import liquidityMenu from '../assets/menu/liquidity_menu.svg';
+
 export const MOBILE_MENU_LINKS = [
   { id: 'home', label: 'home', href: 'https://4teen.me' },
   { id: 'whitepaper', label: 'whitepaper', href: 'https://4teen.me/wp' },
@@ -19,70 +35,70 @@ export const MOBILE_SOCIALS = [
     id: 'facebook',
     shortName: 'facebook',
     href: 'https://facebook.com/Fourteentoken',
-    icon: '/src/assets/socials/facebook_social.svg',
+    icon: facebookSocial,
     alt: 'Facebook'
   },
   {
     id: 'x',
     shortName: 'x',
     href: 'https://x.com/4teentoken',
-    icon: '/src/assets/socials/x_social.svg',
+    icon: xSocial,
     alt: 'X'
   },
   {
     id: 'instagram',
     shortName: 'instagram',
     href: 'https://instagram.com/fourteentoken',
-    icon: '/src/assets/socials/instagram_social.svg',
+    icon: instagramSocial,
     alt: 'Instagram'
   },
   {
     id: 'youtube',
     shortName: 'youtube',
     href: 'https://www.youtube.com/@4teentoken',
-    icon: '/src/assets/socials/youtube_social.svg',
+    icon: youtubeSocial,
     alt: 'YouTube'
   },
   {
     id: 'whatsapp',
     shortName: 'whatsapp',
     href: 'https://wa.me/16462178070',
-    icon: '/src/assets/socials/whatsapp_social.svg',
+    icon: whatsappSocial,
     alt: 'WhatsApp'
   },
   {
     id: 'telegram',
     shortName: 'telegram',
     href: 'https://t.me/fourteentoken',
-    icon: '/src/assets/socials/telegram_social.svg',
+    icon: telegramSocial,
     alt: 'Telegram'
   },
   {
     id: 'threads',
     shortName: 'threads',
     href: 'https://www.threads.com/@fourteentoken',
-    icon: '/src/assets/socials/threads_social.svg',
+    icon: threadsSocial,
     alt: 'Threads'
   },
   {
     id: 'tiktok',
     shortName: 'tiktok',
     href: 'https://www.tiktok.com/@4teentoken',
-    icon: '/src/assets/socials/tiktok_social.svg',
+    icon: tiktokSocial,
     alt: 'TikTok'
   },
   {
     id: 'discord',
     shortName: 'discord',
     href: 'https://discord.gg/jWZF6KzPCB',
-    icon: '/src/assets/socials/discord_social.svg',
+    icon: discordSocial,
     alt: 'Discord'
   },
   {
     id: 'github',
     shortName: 'github',
     href: 'https://github.com/info14fourteen-creator',
-    icon: '/src/assets/socials/github_social.svg',
+    icon: githubSocial,
     alt: 'GitHub'
   }
 ];
@@ -92,25 +108,25 @@ export const MOBILE_BOTTOM_NAV = [
     id: 'buy',
     label: 'buy\ntoken',
     href: 'https://4teen.me/bt',
-    icon: '/src/assets/menu/buy_menu.svg'
+    icon: buyMenu
   },
   {
     id: 'swap',
     label: 'swap\ntoken',
     href: 'https://4teen.me/sw',
-    icon: '/src/assets/menu/swap_menu.svg'
+    icon: swapMenu
   },
   {
     id: 'unlock',
     label: 'unlock\ntimeline',
     href: 'https://4teen.me/ult',
-    icon: '/src/assets/menu/unlock_menu.svg'
+    icon: unlockMenu
   },
   {
     id: 'liquidity',
     label: 'liquidity\ncontroller',
     href: 'https://4teen.me/lc',
-    icon: '/src/assets/menu/liquidity_menu.svg'
+    icon: liquidityMenu
   }
 ];
 
